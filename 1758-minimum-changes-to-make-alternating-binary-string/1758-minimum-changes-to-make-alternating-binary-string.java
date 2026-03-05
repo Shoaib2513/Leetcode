f39486f1f7394b754f46bd40218f8f9c2e9eae32
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-03-05 21:35:17 +0530

    Time: 6 ms (33.91%), Space: 44 MB (18.42%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,31 +1,18 @@
 class Solution {
-    public int operationOne(String s){
-        int count = 0;
-        int n = s.length();
-        for(int i=0;i<n;i++){
-            if(i%2==0){
-                if(s.charAt(i)=='0') count++;
-            }else{
-                if(s.charAt(i)=='1') count++;
-            }
-        }
-        return count;
-    }
-    public int operationZero(String s){
-        int count=0;
-        int n = s.length();
-        for(int i=0;i<n;i++){
-            if(i%2==0){
-                if(s.charAt(i)=='1') count++;
-            }else{
-                if(s.charAt(i)=='0') count++;
-            }
-        }
-        return count;
-    }
     public int minOperations(String s) {
-        int count1 = operationOne(s);
-        int count2 = operationZero(s);
-        return Math.min(count1,count2);
+        int count1 = 0; 
+        int count2 = 0; 
+
+        for(int i = 0; i < s.length(); i++){
+            if(i % 2 == 0){
+                if(s.charAt(i) != '0') count1++;
+                if(s.charAt(i) != '1') count2++;
+            }else{
+                if(s.charAt(i) != '1') count1++;
+                if(s.charAt(i) != '0') count2++;
+            }
+        }
+
+        return Math.min(count1, count2);
     }
 }
