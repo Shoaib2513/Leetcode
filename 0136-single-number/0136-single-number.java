0003637895fb4d3326caa73603597b31e6bf9765
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-03-07 15:58:05 +0530

    Time: 1 ms (99.93%), Space: 46.9 MB (82.39%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            boolean flag = false;
-            for(int j=0;j<nums.length;j++){
-                if(i==j) continue;
-                if(nums[i]==nums[j]){
-                    flag = true;
-                    break;
-                }
-            }
-            if(flag==false) return nums[i];
+        int ans = 0;
+        for(int el:nums){
+            ans ^= el;
         }
-        return 32;
+        return ans;
     }
 }
