58641548857b1aaccd006ce9df680743b4064ee1
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-03-11 09:15:56 +0530

    Time: 1 ms (24.44%), Space: 42.3 MB (32.44%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,31 +1,15 @@
 class Solution {
-    public String convertToBinary(int n){
-        if(n==0) return "0";
-        StringBuilder sb = new StringBuilder();
-        while(n>0){
-            int rem= n%2;
-            sb.append(Integer.toString(rem));
-            n /= 2;
-        }
-        return sb.reverse().toString();
-    }
     public int bitwiseComplement(int n) {
-        String s = convertToBinary(n);
-        StringBuilder sb = new StringBuilder(s);
-        for(int i=0;i<sb.length();i++){
-            if(sb.charAt(i)=='1') sb.setCharAt(i,'0');
-            else sb.setCharAt(i,'1');
+        String str = Integer.toBinaryString(n);
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='0') sb.append('1');
+            else sb.append('0');
         }
-        int ans = 0;
-        int i=sb.length()-1;
-        int x = 1;
-        while(i>=0){
-            if(sb.charAt(i)=='1'){
-                ans += x*1;
-            }
-            x *= 2;
-            i--;
-        }
-        return ans;
+
+        String s = sb.toString();
+        int num = Integer.parseInt(s,2);
+        return num;
     }
 }
