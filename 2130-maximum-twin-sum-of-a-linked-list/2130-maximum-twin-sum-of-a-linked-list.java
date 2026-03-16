072971e8b3d8cd7a2e045873d64c09d909d8a897
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-03-16 11:13:02 +0530

    Time: 8 ms (27.65%), Space: 103.1 MB (28.72%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -1,37 +1,31 @@
+
 class Solution {
     public ListNode reverse(ListNode head){
-        ListNode curr = head;
-        ListNode prev = null;
-        ListNode Next = null;
-        while(curr!=null){
-            Next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = Next;
-        }
-        return prev;
+        if(head==null || head.next==null) return head;
+
+        ListNode newHead = reverse(head.next);
+        head.next.next = head;
+        head.next= null;
+        return newHead;
     }
     public int pairSum(ListNode head) {
-        ListNode temp1 = head;
-        ListNode dummy = new ListNode(-1);
-        ListNode ans = dummy;
-        ListNode temp = head;
-        while(temp!=null){
-            dummy.next = new ListNode(temp.val);
-            dummy = dummy.next;
-            temp = temp.next;
-        }
-        ans = ans.next;
+      ListNode slow = head;
+      ListNode fast = head;
+      while(fast!=null){
+        slow = slow.next;
+        fast = fast.next.next;
+      }         
 
-        ListNode temp2 = reverse(ans);
-        int maxSum = Integer.MIN_VALUE;
+      ListNode temp1 = head;
+      ListNode temp2 = reverse(slow);
+      int maxSum = Integer.MIN_VALUE;
 
-        while(temp1!=null && temp2!=null){
-            int sum = temp1.val + temp2.val;
-            maxSum = Math.max(maxSum,sum);
-            temp1 = temp1.next;
-            temp2 = temp2.next;
-        }
-        return maxSum;
+      while(temp1!=slow){
+        int sum = temp1.val+temp2.val;
+        maxSum = Math.max(maxSum,sum);
+        temp1 = temp1.next;
+        temp2 = temp2.next;
+      }
+      return maxSum;
     }
 }
