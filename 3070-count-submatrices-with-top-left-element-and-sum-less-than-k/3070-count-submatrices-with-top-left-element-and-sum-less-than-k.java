319f3ca7f3c60444af89ff53126bb3d9f1bbe32c
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-03-18 16:08:28 +0530

    Time: 3 ms (100%), Space: 161.2 MB (63.71%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -3,19 +3,17 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        int[][] prefix = new int[m][n];
+        int[] colSum = new int[n];
         int count = 0;
 
         for(int i = 0; i < m; i++){
+            int rowSum = 0;
+
             for(int j = 0; j < n; j++){
-                
-                int up = (i > 0) ? prefix[i - 1][j] : 0;
-                int left = (j > 0) ? prefix[i][j - 1] : 0;
-                int diag = (i > 0 && j > 0) ? prefix[i - 1][j - 1] : 0;
+                colSum[j] += grid[i][j];
+                rowSum += colSum[j];
 
-                prefix[i][j] = grid[i][j] + up + left - diag;
-
-                if(prefix[i][j] <= k){
+                if(rowSum <= k){
                     count++;
                 }
             }
