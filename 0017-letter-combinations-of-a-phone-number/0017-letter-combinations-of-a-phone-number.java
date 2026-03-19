f463d29d3582c5e57e11c8f6c1e42a7ab5358917
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-03-19 20:50:31 +0530

    Time: 0 ms (100%), Space: 43.6 MB (49.01%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,14 +1,16 @@
 class Solution {
-    public void solve(int idx, String digits, String[] map, String curr, List<String> ans) {
+    public void solve(int idx, String digits, String[] map, StringBuilder curr, List<String> ans) {
         if (idx == digits.length()) {
-            ans.add(curr);
+            ans.add(curr.toString());
             return;
         }
 
         String letters = map[digits.charAt(idx) - '0'];
 
         for (int i = 0; i < letters.length(); i++) {
-            solve(idx + 1, digits, map, curr + letters.charAt(i), ans);
+            curr.append(letters.charAt(i));   
+            solve(idx + 1, digits, map, curr, ans);
+            curr.deleteCharAt(curr.length() - 1); 
         }
     }
 
@@ -22,7 +24,7 @@ class Solution {
             "jkl", "mno", "pqrs", "tuv", "wxyz"
         };
 
-        solve(0, digits, map, "", ans);
+        solve(0, digits, map, new StringBuilder(), ans);
 
         return ans;
     }
