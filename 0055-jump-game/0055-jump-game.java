92f8d2d6292d633ec08c7c2e8545cf6347817bb7
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-03-21 21:04:23 +0530

    Time: 2 ms (85.59%), Space: 48.1 MB (35.61%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -5,10 +5,7 @@ class Solution {
         
         for(int i = 0; i < nums.length; i++){
             if(i > maxReach) return false;
-    
             maxReach = Math.max(maxReach, i + nums[i]);
-    
-            if(maxReach >= nums.length - 1) return true;
         }
         
         return true;
