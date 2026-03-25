b0e541cddd1f650dc3359d1d74ea0c5f1b146142
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-03-25 13:03:14 +0530

    Time: 4 ms (97.2%), Space: 160.4 MB (89.51%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -4,27 +4,27 @@ class Solution {
         int n = grid[0].length;
 
         long totalSum = 0;
+
+        long[] rowSum = new long[m];
+        long[] colSum = new long[n];
+
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
+                rowSum[i] += grid[i][j];
+                colSum[j] += grid[i][j];
                 totalSum += grid[i][j];
             }
         }
         long currSum = 0;
         for(int i = 0; i < m - 1; i++){
-            for(int j = 0; j < n; j++){
-                currSum += grid[i][j];
-            }
-
+            currSum += rowSum[i];
             if(currSum == totalSum - currSum){
                 return true;
             }
         }
         currSum = 0;
         for(int j = 0; j < n - 1; j++){
-            for(int i = 0; i < m; i++){
-                currSum += grid[i][j];
-            }
-
+            currSum += colSum[j];
             if(currSum == totalSum - currSum){
                 return true;
             }
