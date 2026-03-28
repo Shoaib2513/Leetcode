4125c78bfb1e792dbd43bdea262f0c593440f7f2
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-03-28 17:12:29 +0530

    Time: 15 ms (17.14%), Space: 178.2 MB (42.86%) - LeetHub

--- 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.java
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.java
@@ -9,60 +9,33 @@ class Solution {
             }
         }
 
-        int[] parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (lcp[i][j] > 0) {
-                    union(i, j, parent);
-                }
-            }
-        }
-
         char[] res = new char[n];
         char ch = 'a';
 
         for (int i = 0; i < n; i++) {
-            int p = find(i, parent);
-            if (res[p] == 0) {
-                if (ch > 'z') return "";
-                res[p] = ch++;
+            if (res[i] != 0) continue;
+            if (ch > 'z') return "";
+            res[i] = ch;
+            for (int j = i + 1; j < n; j++) {
+                if (lcp[i][j] > 0) res[j] = ch;
             }
-            res[i] = res[p];
+            ch++;
         }
 
-        int[][] calc = new int[n][n];
+        int[][] dp = new int[n + 1][n + 1];
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if (res[i] == res[j]) {
-                    if (i == n - 1 || j == n - 1) {
-                        calc[i][j] = 1;
-                    } else {
-                        calc[i][j] = 1 + calc[i + 1][j + 1];
-                    }
-                }
+                if (res[i] == res[j]) dp[i][j] = 1 + dp[i + 1][j + 1];
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (calc[i][j] != lcp[i][j]) return "";
+                if (dp[i][j] != lcp[i][j]) return "";
             }
         }
 
         return new String(res);
-    }
-
-    private int find(int x, int[] parent) {
-        if (parent[x] != x) parent[x] = find(parent[x], parent);
-        return parent[x];
-    }
-
-    private void union(int a, int b, int[] parent) {
-        int pa = find(a, parent);
-        int pb = find(b, parent);
-        if (pa != pb) parent[pa] = pb;
     }
 }
