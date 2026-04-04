2b4d3c84bcd2d098c7af54f5169d5b8800adad02
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-04-04 13:10:44 +0530

    Time: 4 ms (100%), Space: 106.1 MB (76.57%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -1,35 +1,38 @@
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        
-        int n = 0;
-        ListNode temp = head;
-        while(temp != null){
-            temp = temp.next;
-            n++;
-        }
-
-        temp = head;
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++){
-            arr[i] = temp.val;
-            temp = temp.next;
-        }
-
         int[] ans = {-1, -1};
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 1; i < n - 1; i++){
-            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]) list.add(i);
-            if(arr[i] < arr[i-1] && arr[i] < arr[i+1]) list.add(i);
+        
+        if (head == null || head.next == null || head.next.next == null) return ans;
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+        int index = 1;
+
+        int first = -1, last = -1;
+        int minDist = Integer.MAX_VALUE;
+
+        while (curr.next != null) {
+            if ((curr.val > prev.val && curr.val > curr.next.val) ||
+                (curr.val < prev.val && curr.val < curr.next.val)) {
+
+                if (first == -1) {
+                    first = index;
+                } else {
+                    minDist = Math.min(minDist, index - last);
+                }
+                last = index;
+            }
+
+            prev = curr;
+            curr = curr.next;
+            index++;
         }
-        if(list.size() < 2) return ans;
-        int res = Integer.MAX_VALUE;
-        for(int i = 1; i < list.size(); i++){
-            int diff = list.get(i) - list.get(i-1);
-            res = Math.min(diff, res);
-        }
-        int mx = list.get(list.size()-1) - list.get(0);
-        ans[0] = res;
-        ans[1] = mx;
+
+        if (first == last) return ans;
+
+        ans[0] = minDist;
+        ans[1] = last - first;
+
         return ans;
     }
 }
