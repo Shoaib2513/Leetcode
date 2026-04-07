f5f4de7e27f7a773db29dd7fe1e3844119bd79eb
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-04-07 18:20:38 +0530

    Time: 57 ms (99.3%), Space: 61.1 MB (5.59%) - LeetHub

--- 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
@@ -1,42 +1,49 @@
 class Robot {
 
-    int width, height, perimeter, steps;
+    int width, height;
+    int perimeter;
+    int stepsTaken;
     boolean moved;
 
     public Robot(int width, int height) {
         this.width = width;
         this.height = height;
         this.perimeter = 2 * (width + height) - 4;
+        this.stepsTaken = 0;
+        this.moved = false;
     }
     
     public void step(int num) {
-        steps = (steps + num) % perimeter;
+        stepsTaken = (stepsTaken + num) % perimeter;
         moved = true;
     }
     
     public int[] getPos() {
-        int s = steps;
+        int s = stepsTaken;
 
         if (s < width) return new int[]{s, 0};
+        s -= width;
 
-        if (s < width + height - 1)
-            return new int[]{width - 1, s - width + 1};
+        if (s < height - 1) return new int[]{width - 1, s + 1};
+        s -= (height - 1);
 
-        if (s < 2 * width + height - 2)
-            return new int[]{width - 1 - (s - (width + height - 1)), height - 1};
+        if (s < width - 1) return new int[]{width - 2 - s, height - 1};
+        s -= (width - 1);
 
-        return new int[]{0, height - 1 - (s - (2 * width + height - 2))};
+        return new int[]{0, height - 2 - s};
     }
     
-    public String getDir() {
-        if (steps == 0) return moved ? "South" : "East";
+   public String getDir() {
+    int[] pos = getPos();
+    int x = pos[0], y = pos[1];
 
-        if (steps < width - 1) return "East";
-
-        if (steps < width + height - 2) return "North";
-
-        if (steps < 2 * width + height - 3) return "West";
-
-        return "South";
+    if (x == 0 && y == 0) {
+        return moved ? "South" : "East";
     }
+
+    if (y == 0) return "East";
+    if (x == width - 1) return "North";
+    if (y == height - 1) return "West";
+    return "South";
+}
 }
