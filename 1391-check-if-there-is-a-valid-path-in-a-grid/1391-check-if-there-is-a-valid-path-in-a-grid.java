3ca13bf8a0efa79c5e0d6dfcb26c516f8a278e81
Shoaib Akhtar <118381420+Shoaib2513@users.noreply.github.com>
2026-04-27 20:21:02 +0530

    Time: 24 ms (53.39%), Space: 88.7 MB (82.71%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.java
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.java
@@ -3,20 +3,12 @@ class Solution {
         int m = grid.length, n = grid[0].length;
         
         boolean[][] vis = new boolean[m][n];
-        
-        int[][][] dirs = {
-            {},
-            {{0,-1},{0,1}},
-            {{-1,0},{1,0}},
-            {{0,-1},{1,0}},
-            {{0,1},{1,0}},
-            {{0,-1},{-1,0}},
-            {{0,1},{-1,0}}
-        };
-        
         java.util.Queue<int[]> q = new java.util.LinkedList<>();
+        
         q.add(new int[]{0,0});
         vis[0][0] = true;
+        
+        int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
         
         while(!q.isEmpty()){
             int[] cur = q.poll();
@@ -24,21 +16,36 @@ class Solution {
             
             if(r == m-1 && c == n-1) return true;
             
-            for(int[] d : dirs[grid[r][c]]){
+            for(int[] d : dir){
                 int nr = r + d[0];
                 int nc = c + d[1];
                 
                 if(nr < 0 || nc < 0 || nr >= m || nc >= n || vis[nr][nc]) continue;
                 
-                for(int[] back : dirs[grid[nr][nc]]){
-                    if(nr + back[0] == r && nc + back[1] == c){
-                        vis[nr][nc] = true;
-                        q.add(new int[]{nr, nc});
-                        break;
-                    }
+                if(isValid(grid[r][c], grid[nr][nc], d)){
+                    vis[nr][nc] = true;
+                    q.add(new int[]{nr, nc});
                 }
             }
         }
+        
+        return false;
+    }
+    
+    private boolean isValid(int a, int b, int[] d){
+        int dr = d[0], dc = d[1];
+        
+        if(dr == 0 && dc == 1)
+            return (a==1||a==4||a==6) && (b==1||b==3||b==5);
+        
+        if(dr == 0 && dc == -1)
+            return (a==1||a==3||a==5) && (b==1||b==4||b==6);
+        
+        if(dr == 1 && dc == 0)
+            return (a==2||a==3||a==4) && (b==2||b==5||b==6);
+        
+        if(dr == -1 && dc == 0)
+            return (a==2||a==5||a==6) && (b==2||b==3||b==4);
         
         return false;
     }
